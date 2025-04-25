980af3466ecce617c12b32a4ebc16fa25936e18d
Mansub Hafeez <mansubsheikh4@gmail.com>
2025-04-25 19:30:36 +0500

    updations

--- explore_page.py
+++ explore_page.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def shorten_categories(categories, cutoff):
     categorical_map = {}
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,9 @@ import streamlit as st
 from predict_page import show_predict_page
 from explore_page import show_explore_page
 
-# Sidebar selection should come BEFORE showing any page
+
 page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
 
-# Show the selected page
 if page == "Predict":
     show_predict_page()
 else:
